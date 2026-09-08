61882fdbbfba1042fdfd1da526ae7e652c9d6013
nkosi <nkoc.mkhize18@gmail.com>
2026-09-08 18:03:29 +0200

    Fix workflow

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -7,6 +7,11 @@ resource "aws_iam_openid_connect_provider" "github" {
     "sts.amazonaws.com"
   ]
 
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
+
   tags = {
     Name = "${var.project_name}-github-oidc"
   }
