23f7686bfe7ad0b8b359a08023fff6affbdb596a
CodeWithNkosi <nkoc.mkhize18@gmail.com>
2026-09-08 13:16:36 +0200

    feat: add public subnets and internet gateway (#4)

--- vpc.tf
+++ vpc.tf
@@ -12,6 +12,10 @@ resource "aws_vpc" "main" {
   }
 }
 
+# -------------------------
+# Private subnets
+# -------------------------
+
 resource "aws_subnet" "private" {
   count = 2
 
@@ -39,4 +43,55 @@ resource "aws_route_table_association" "private" {
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
+}
+
+# -------------------------
+# Internet Gateway
+# -------------------------
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+# -------------------------
+# Public subnets
+# -------------------------
+
+resource "aws_subnet" "public" {
+  count = 2
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 10)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-${count.index + 1}"
+    Tier = "public"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
