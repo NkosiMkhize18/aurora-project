af27e9a59a99f567259a0b77826d01ef83306a71
nkosi <nkoc.mkhize18@gmail.com>
2026-09-08 18:09:09 +0200

    TRun

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -45,7 +45,7 @@ data "aws_iam_policy_document" "github_plan_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_repository}:pull_request"
+        "repo:NkosiMkhize18@19518913/aurora-project@1360545813:pull_request"
       ]
 
     }
@@ -80,7 +80,7 @@ data "aws_iam_policy_document" "github_apply_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_repository}:ref:refs/heads/main"
+        "repo:NkosiMkhize18@19518913/aurora-project@1360545813:ref:refs/heads/main"
       ]
     }
   }
