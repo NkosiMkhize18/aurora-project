c372b7fedb30e7efd38992f8d0197ccb2601ff8d
CodeWithNkosi <nkoc.mkhize18@gmail.com>
2026-09-08 16:05:55 +0200

    Refactoring (#7)
    * Refactoring
    
    * Refactoring outputs.tf to include public subnet IDs and availability zones.

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "private_subnet_ids" {
   value       = aws_subnet.private[*].id
 }
 
+output "public_subnet_ids" {
+  description = "IDs of the public subnets."
+  value       = aws_subnet.public[*].id
+}
+
 output "availability_zones" {
   description = "Availability zones used by the private subnets."
   value       = aws_subnet.private[*].availability_zone
